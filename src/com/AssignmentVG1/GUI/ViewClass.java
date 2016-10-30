c564f3e028c309705a02ddf0b5feaa8b0b2d2a48
mohbes <mohbes@IMM.INTRA>
2016-10-30 14:33:03 +0100

    Commit 6: I have changed the textfield to a label botton and modified the uml. The project almost is complete, Maybe I will add some other functions.

--- src/com/AssignmentVG1/GUI/ViewClass.java
+++ src/com/AssignmentVG1/GUI/ViewClass.java
@@ -5,18 +5,13 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
-
 import com.AssignmentVG1.methods.PlayerMethods;
-
 import jaco.mp3.player.MP3Player;
-
 /**
  * 
  * Class ViewClass for creating the GUI
@@ -26,27 +21,25 @@ import jaco.mp3.player.MP3Player;
  * @version 1.0
  **/
 
-public class ViewClass implements ActionListener{
-	private File f = new File("");
+public class ViewClass implements ActionListener {
 	private PlayerMethods pMethods = new PlayerMethods();
 	private JFileChooser fc = new JFileChooser();
-	
 	private MP3Player player = new MP3Player(new File(""));
 	private JFrame frmMyMusicPlayer;
-	private JTextField textField;
 	private JButton btnOpen = new JButton("");
 	private JButton btnPlay = new JButton("");
 	private JButton btnPause = new JButton("");
 	private JButton btnStop = new JButton("");
-	JLabel lblNewLabel = new JLabel("Music:");
+	private JLabel lblMusic = new JLabel("Music:");
+	private JLabel lblMusicName = new JLabel("");
+
 	/**
 	 * Constructor of the ViewClass that invokes the method 'initialize()';
-
 	 * 
 	 * @param none
 	 * @return none
 	 */
-	
+
 	public ViewClass() {
 		initialize();
 		addComponentToForm();
@@ -69,122 +62,128 @@ public class ViewClass implements ActionListener{
 		/***
 		 * creating buttons and assigning image to them
 		 */
-//		JButton btnOpen = new JButton();
 		btnOpen.setIcon(new ImageIcon("C:\\Users\\mohbes\\workspace\\VG-Assignment1\\Icon\\openp.jpg"));
 		btnOpen.setBounds(12, 85, 76, 25);
-		
-//		JButton btnPlay = new JButton();
-		btnPlay.setIcon(new ImageIcon("C:\\Users\\mohbes\\workspace\\VG-Assignment1\\Icon\\playp.jpg"));	
+
+		btnPlay.setIcon(new ImageIcon("C:\\Users\\mohbes\\workspace\\VG-Assignment1\\Icon\\playp.jpg"));
 		btnPlay.setBounds(123, 85, 76, 25);
 
-//		JButton btnPause = new JButton();
 		btnPause.setIcon(new ImageIcon("C:\\Users\\mohbes\\workspace\\VG-Assignment1\\Icon\\pausep.jpg"));
 		btnPause.setBounds(225, 85, 82, 25);
-		
-//		JButton btnStop = new JButton();
+
 		btnStop.setIcon(new ImageIcon("C:\\Users\\mohbes\\workspace\\VG-Assignment1\\Icon\\stopp.jpg"));
 		btnStop.setBounds(331, 85, 76, 25);
 		/***
 		 * This part creates a label
 		 */
-		//JLabel lblNewLabel = new JLabel("Music:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel.setBackground(new Color(192, 192, 192));
-		lblNewLabel.setBounds(32, 13, 66, 16);
+		lblMusic.setForeground(new Color(0, 0, 0));
+		// JLabel lblNewLabel = new JLabel("Music:");
+		lblMusic.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblMusic.setBackground(new Color(192, 192, 192));
+		lblMusic.setBounds(32, 13, 66, 16);
 		/***
 		 * This part creates a TextField to show the music name
 		 */
-		textField = new JTextField();
-		textField.setBounds(96, 12, 325, 22);
-		frmMyMusicPlayer.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		
-	}
-		/***
-		 *  adding buttons, the label and textField to Frame
-		 */
+		lblMusicName.setForeground(Color.YELLOW);
+		lblMusicName.setFont(
+		lblMusicName.getFont().deriveFont(lblMusicName.getFont().getStyle() | Font.BOLD | Font.ITALIC));
+		lblMusicName.setBackground(new Color(192, 192, 192));
+		lblMusicName.setBounds(112, 13, 295, 16);
 
-		public void addComponentToForm(){
-			frmMyMusicPlayer.getContentPane().add(btnOpen);
-			frmMyMusicPlayer.getContentPane().add(btnPlay);
-			frmMyMusicPlayer.getContentPane().add(btnPause);
-			frmMyMusicPlayer.getContentPane().add(btnStop);
-			frmMyMusicPlayer.getContentPane().add(lblNewLabel);
-			frmMyMusicPlayer.setVisible(true);
-		}
-		
-		/**
-		 * Method for adding the action listeners
-		 * 
-		 * @param none
-		 * @return none
-		 */
-		public void addActionListerners(){			
-			btnOpen.addActionListener(this) ;
-			btnPlay.addActionListener(this);
-			btnPause.addActionListener(this);
-			btnStop.addActionListener(this);
-		}
-		
-		/**
-		 * Implementation of the action performed for handling the actions on the
-		 * Mp3Player
-		 * 
-		 * @param e
-		 * @return none
-		 */
-		public void actionPerformed(ActionEvent e) {
-			
+	}
+
+	/***
+	 * adding buttons, the label and textField to Frame
+	 */
+
+	public void addComponentToForm() {
+		frmMyMusicPlayer.getContentPane().add(btnOpen);
+		frmMyMusicPlayer.getContentPane().add(btnPlay);
+		frmMyMusicPlayer.getContentPane().add(btnPause);
+		frmMyMusicPlayer.getContentPane().add(btnStop);
+		frmMyMusicPlayer.getContentPane().add(lblMusic);
+
+		frmMyMusicPlayer.getContentPane().add(lblMusicName);
+
+		frmMyMusicPlayer.setVisible(true);
+	}
+
+	/**
+	 * Method for adding the action listeners
+	 * 
+	 * @param none
+	 * @return none
+	 */
+	public void addActionListerners() {
+		btnOpen.addActionListener(this);
+		btnPlay.addActionListener(this);
+		btnPause.addActionListener(this);
+		btnStop.addActionListener(this);
+	}
+
+	/**
+	 * Implementation of the action performed for handling the actions on the
+	 * Mp3Player
+	 * 
+	 * @param e
+	 * @param lblMusicName
+	 * @return none
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
 		/***
 		 * in case you click on open and click on a music name
 		 */
+		btnPlay.setBackground(new Color(240, 240, 240));
+		btnPause.setBackground(new Color(240, 240, 240));
+		btnStop.setBackground(new Color(240, 240, 240));
 
-			if (e.getSource() == btnOpen) {
-					int returnVal = fc.showOpenDialog(btnOpen);
-					if (returnVal == JFileChooser.APPROVE_OPTION) {
-						File f = fc.getSelectedFile();
-						player = new MP3Player(f);
-						pMethods.stop();
-						btnStop.setBackground(Color.cyan);
+		if (e.getSource() == btnOpen) {
+			int returnVal = fc.showOpenDialog(btnOpen);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File f = fc.getSelectedFile();
+				player = new MP3Player(f);
 
-						pMethods.setPlayer(player);
-						String name = f.getName();
-						textField.setText(name);
-						pMethods.play();
-						btnPlay.setBackground(Color.cyan);
-					}
-			}	
-			/***
-			 * in case you click on play, it will play the last music
-			 *  which has been selected or paused/stopped
-			 */	
-			if (e.getSource() == btnPlay) {
-				pMethods.setPlayer(player);
-				pMethods.play();
-/*				btnPlay.setBackground(Color.green);
-*/			}
-
-			/***
-			 * in case you click on pause, it will pause the  music
-			 *  which is playing
-			 */	
-			if (e.getSource() == btnPause ) {
-				pMethods.pause();
-				btnPlay.setBackground(Color.cyan);
-				//btnPlay.setBackground();
-
-			}
-			/***
-			 * in case you click on stop, it will stop the  music
-			 *  which is playing
-			 */	
-			if (e.getSource() == btnStop ) {
 				pMethods.stop();
-				btnPlay.setBackground(Color.BLACK);
+				btnPlay.setBackground(null);
+				pMethods.setPlayer(player);
+				String name = f.getName();
+				lblMusicName.setText(name);
+				pMethods.play();
+				btnPlay.setBackground(Color.GREEN);
 			}
-			
 		}
-	
+		/***
+		 * in case you click on play, it will play the last music which has been
+		 * selected or paused/stopped
+		 */
+		if (e.getSource() == btnPlay) {
+			pMethods.setPlayer(player);
+			pMethods.play();
+			if (!lblMusicName.getText().isEmpty())
+				btnPlay.setBackground(Color.green);
+		}
+
+		/***
+		 * in case you click on pause, it will pause the music which is playing
+		 */
+		if (e.getSource() == btnPause) {
+			pMethods.pause();
+			btnPause.setBackground(Color.GRAY);
+			btnPlay.setBackground(new Color(240, 240, 240));
+
+		}
+		/***
+		 * in case you click on stop, it will stop the music which is playing
+		 */
+		if (e.getSource() == btnStop) {
+			pMethods.stop();
+			btnStop.setBackground(Color.GRAY);
+			btnPlay.setBackground(new Color(240, 240, 240));
+
+		}
+
 	}
 
+}
--- src/com/AssignmentVG1/main/Main.java
+++ src/com/AssignmentVG1/main/Main.java
@@ -1,24 +1,10 @@
 package com.AssignmentVG1.main;
-import java.awt.EventQueue;
 
 import com.AssignmentVG1.GUI.ViewClass;
 
 public class Main {
-	/*private JFrame frmMyMusicPlayer;
-	private ViewClass vClass;*/
 
 	public static void main(String[] args) {
-		ViewClass window = new ViewClass();
-
-		// EventQueue.invokeLater(new Runnable() {
-		// public void run() {
-		// try {
-		// ViewClass window = new ViewClass();
-		// window.frmViewClass.setVisible(true);
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// });
+		new ViewClass();
 	}
 }
